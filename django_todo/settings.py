333e58a1cc98528535ca7a27c615934df33c967b
Niall Hobson <greenflash1555@gmail.com>
2019-04-24 19:00:44 +0000

    SECRET_KEY Settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$w0qz1%bim=^6+o%%jelky^6bi8x+!eiy(900dc47^r_urck%1'
+#SECRET_KEY = '$w0qz1%bim=^6+o%%jelky^6bi8x+!eiy(900dc47^r_urck%1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
